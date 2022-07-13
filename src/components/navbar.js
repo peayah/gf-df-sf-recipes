bdd1499b66f50528935f2ea116d76fc3cfd86645
peayah <82358003+peayah@users.noreply.github.com>
2022-07-12 19:22:40 -0700

    add li to nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,10 +5,18 @@ const Navbar = () => {
   return (
     <nav>
         <ul>
+          <li>
             <Link to="/">Home</Link>
+            </li>
+            <li>
             <Link to="/about">About</Link>
+            </li>
+            <li>
             <Link to="/contact">Contact</Link>
+            </li>
+            <li>
             <Link to="/recipes/">Recipes</Link>
+            </li>
         </ul>
 
 
