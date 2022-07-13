6435c97f1771ac064601b4be793b262f4918246f
peayah <82358003+peayah@users.noreply.github.com>
2022-07-12 22:57:44 -0700

    add show hide functionality to nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,38 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link} from "gatsby"
 import logo from "../assets/images/logo.svg"
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
     <nav className='navbar'>
       <div className="nav-center">
         <div className="nav-header">
           <Link to ="/">
-            <img src = { logo } alt ="favorite recipes"></img>
-          </Link>
-          <button className='nav-btn'>
+            <img src = { logo } alt ="favorite recipes"/>
+          </Link> 
+          <button className='nav-btn' onClick={() => setShow(!show)}>
             =
           </button>
         </div>
-        <div className="nav-links show-links">
-          <li>
-            <Link to="/" className='nav-link ' activeClassName='active-link'>Home</Link>
-            </li>
-
-            <li>
-            <Link to="/about" className='nav-link ' activeClassName='active-link'>About</Link>
-            </li>
-            <li>
-            <Link to="/contact" className='nav-link ' activeClassName='active-link'>Contact</Link>
-            </li>
-            <li>
-            <Link to="/recipes/" className='nav-link ' activeClassName='active-link'>Recipes</Link>
-            </li></div> 
+        <div className={show ? 'nav-links show-links' : "nav-links"}
+        >
+            <Link to="/" 
+            className='nav-link' 
+            activeClassName='active-link'
+            onClick={() => setShow(false)}
+            >
+              Home
+            </Link>
+            <Link to="/about" 
+            className='nav-link' 
+            activeClassName='active-link' 
+            onClick={() => setShow(false)}
+            >
+              About
+            </Link>
+            <Link to="/contact" 
+            className='nav-link' 
+            activeClassName='active-link' 
+            onClick={() => setShow(false)}
+            >
+              Contact
+            </Link>
+            <Link to="/recipes/" 
+            className='nav-link' 
+            activeClassName='active-link' 
+            onClick={() => setShow(false)}
+            >
+              Recipes
+            </Link>
+        </div> 
       </div>
-
-          
-        
-
-
     </nav>   
   )
 }
