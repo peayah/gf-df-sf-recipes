b266f7065e63d6d271bcaaf90ae0d8d5773b05a9
peayah <82358003+peayah@users.noreply.github.com>
2022-07-12 19:51:53 -0700

    add error page structore

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,13 @@ import Layout from '../components/Layout'
 
 const Error = () => {
   return (
-    <Layout>404</Layout>
+    <Layout>
+      <main className ="error-page">
+       <section>
+         <h1>404</h1>
+         <h3>page not found</h3>
+       </section>
+        </main></Layout>
   )
 }
 
