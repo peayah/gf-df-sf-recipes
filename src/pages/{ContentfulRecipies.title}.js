308e08364190e87d83218416dd04f8407f086d5f
peayah <82358003+peayah@users.noreply.github.com>
2022-07-16 20:49:17 -0700

    add dynamic page template

---
+++ src/pages/{ContentfulRecipies.title}.js
@@ -0,0 +1,11 @@
+import React from "react";
+ 
+const RecipeTemplate = props => {
+  return (
+    <div className="page">
+        <h2>props.params.title</h2>
+        </div>
+  )
+}
+
+export default RecipeTemplate
