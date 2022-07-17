ff738c5cc8421bfb18a3c45ecaa7c58c5363f6fa
peayah <82358003+peayah@users.noreply.github.com>
2022-07-17 00:53:17 -0700

    set up recipe page

--- src/pages/{ContentfulRecipies.title}.js
+++ src/pages/{ContentfulRecipies.title}.js
@@ -1,33 +1,137 @@
 import React from "react";
-import { graphql } from "gatsby";
- 
-const RecipeTemplate = props => {
+import { graphql, Link} from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { BsClockHistory, BsPeople, BsClock} from "react-icons/bs";
+import Layout from "../components/Layout";
+const RecipeTemplate = ({data}) => {
+  const {title,
+  cooktime,
+content,
+preptime,
+servings,
+desc,
+image,} = data.contentfulRecipies ;
+const pathToImage = getImage(image)
+const {tags, instructions, ingredients, tools} = content;
   return (
-    <div className="page">
-        <h2>{props.params.title}</h2>
-        </div>
+    <Layout>
+    <main className="page">
+      <div className="recipe-page">
+        {/* hero */}
+        <section className="recipe-hero">
+          <GatsbyImage 
+          image={pathToImage}
+          alt={title}
+          className="about-img"
+          />
+        <article className="recipe-info">
+          <h2>{title}</h2>
+          <p>{desc}</p>
+
+          {/* icons */}
+          <div className="recipe-icons">
+            <article>
+              <BsClock/>
+              <h5>prep time</h5>
+              <p>{preptime}min.</p>
+            </article>
+
+            <article>
+              <BsClockHistory/>
+              <h5>cook time</h5>
+              <p>{cooktime}min.</p>
+            </article>
+
+            <article>
+              <BsPeople/>
+              <h5>servings</h5>
+              <p>{servings}</p>
+            </article>
+            
+          </div>
+
+          {/* tags */}
+          <p className="recipe-tags">
+            Tags: 
+            {tags.map((tag, index)=> {
+              return (
+              <Link to={`/${tag}`} key={index}>
+                {tag}
+              </Link>
+              )
+            })}
+          </p>
+        </article>
+        </section>
+        {/* rest of the content */}
+        <section className="recipe-content">
+          <article>
+            <h4>instructions</h4>
+            {instructions.map((item, index) => {
+              return (
+              <div key={index} 
+              className="single-instruction">
+                <header>
+                  <p>step {index + 1}</p>
+                  <div></div>
+                </header>
+                <p>{item}</p>
+              </div>
+              )
+            })}
+          </article>
+          <article className="second-column">
+            <div>
+              <h4>ingredients</h4>
+              {ingredients.map((item, index) => {
+                return (
+                  <p key={index}
+                  className="single-ingredient">
+                    {item}
+                  </p>
+                )
+              })}
+            </div>
+            <div>
+              <h4>tools</h4>
+              {tools.map((item, index) => {
+                return (
+                  <p key={index}
+                  className="single-tool">
+                    {item}
+                  </p>
+                )
+              })}
+            </div>
+          </article>
+        </section>
+      </div>
+    </main>
+    </Layout>
   )
 }
 
 export const query = graphql`
-  {
-    contentfulRecipies(title: {eq: $title}) {
-      title
-      content {
-        tools
-        ingredients
-        tags
-        instructions
-      }
-      cooktime
-      preptime
-      servings
-      description
-      image {
-        gatsbyImageData(layout: CONSTRAINED, placeholder: DOMINANT_COLOR )
-      }
+  query getSingleRecipe($title: String) {
+  contentfulRecipies(title: {eq: $title}) {
+    title
+    cooktime
+    desc
+    id
+    servings
+    referrence
+    preptime
+    content {
+      ingredients
+      tags
+      tools
+      instructions 
+    }
+    image {
+      gatsbyImageData(placeholder: DOMINANT_COLOR, layout: CONSTRAINED)
     }
   }
+}
 `
 
 export default RecipeTemplate
